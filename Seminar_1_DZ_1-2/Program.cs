497f39d591cf7940042b3b71acad75ea46126a09
ZadRotaS <ops0728@gmail.com>
2022-08-11 19:12:04 +0500

    Сделал 1 и 2 задачу

---
+++ Seminar_1_DZ_1-2/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[m];
+int i = 0;
+while (true)
+{
+    if (i < m)
+    {
+        Console.Write("Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        i += 1;
+    }
+    else
+    {
+        break;
+    }
+}
+i = 0;
+int max = 0;
+while (i < m)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+        i += 1;
+    }
+    else
+    {
+        i += 1;
+    }
+}
+Console.Write("максимальное число: " + max.ToString());
