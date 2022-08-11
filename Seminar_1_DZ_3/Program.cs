270eda96e7569c77a1499d17f41703e6f6d7df77
ZadRotaS <ops0728@gmail.com>
2022-08-11 19:46:10 +0500

    сделал решение 3 задачи

---
+++ Seminar_1_DZ_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+string res = "A";
+if (N % 2 == 0)
+{
+    res = "четным";
+}
+else
+{
+    res = "нечетным";
+}
+Console.WriteLine("Число: " + N.ToString() + " является " + res.ToString() );
