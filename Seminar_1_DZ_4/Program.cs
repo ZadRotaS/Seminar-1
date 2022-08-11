56a15e04c909a363e080c9463adc1d40837cc19d
ZadRotaS <ops0728@gmail.com>
2022-08-11 20:21:54 +0500

    Сделал 4 задачу

---
+++ Seminar_1_DZ_4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int I = N / 2;
+int [] array = new int [I + 1];
+I = 0;
+while (i < N)
+{
+    if (i % 2 == 0)
+    {
+        array[I] = i;
+        I += 1;
+        i += 1;
+    }
+    else
+    {
+        i += 1;
+    }
+}
+I = 0;
+Console.Write("Все четные числа, чила до " + N.ToString() + " это: " );
+while (I < (N / 2))
+{
+        Console.Write(array[I] + ",");
+        I += 1;
+}
