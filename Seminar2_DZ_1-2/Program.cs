f5fa8c8c5653257414a07ac6c8654d2a0032207e
ZadRotaS <ops0728@gmail.com>
2022-08-12 18:24:29 +0500

    Сделал задачу 2(1 и 2)

---
+++ Seminar2_DZ_1-2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if(x > 9)
+{
+    int a = int.Parse(Convert.ToString(x)[1].ToString());
+    Console.WriteLine("Вторая цифра этого числа: " + a);
+}
+else
+{
+    Console.WriteLine("Второй цифры нет");
+}
+if(x > 99)
+{
+    int b = int.Parse(Convert.ToString(x)[2].ToString());
+    Console.WriteLine("Третья цифра этого числа: " + b);
+}
+else 
+{
+    Console.WriteLine("Третей цифры нет");
+}
