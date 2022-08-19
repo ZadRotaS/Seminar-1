a2736155833bfb7f86c4ae3a5e6765dc4dbfd5c1
ZadRotaS <ops0728@gmail.com>
2022-08-19 18:12:49 +0500

    Сделал задачу 3(1)

---
+++ Seminar3_DZ_1/Program.cs
@@ -0,0 +1,19 @@
+﻿int i, x = 0, n = 0;
+Console.Write("Введите число: ");
+int N = Convert.ToInt32((Console.ReadLine()));
+for (i = N; i > 0; i = (i / 10))
+    {
+            x = i % 10;
+            n = n * 10 + x;
+
+    }
+Console.WriteLine(n);
+if(N==n)
+{
+    Console.WriteLine("число: " + N + " является палиндромом");
+}
+else 
+{
+    Console.WriteLine("число: " + N + " неявляется палиндромом");
+}
+
