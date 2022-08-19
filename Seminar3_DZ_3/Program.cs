435f7bdbf5c133da813611a5552567226906f465
ZadRotaS <ops0728@gmail.com>
2022-08-19 17:18:39 +0500

    сделал задачу 3(3)

---
+++ Seminar3_DZ_3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: "); int N = Convert.ToInt32(Console.ReadLine());
+Console.Write(N + "-> ");
+for (int i=1; i < N + 1; i++)
+{
+    int n = i * i * i;
+    Console.Write(n + ",");
+}
